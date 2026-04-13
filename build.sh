066f810f6930b0f098e295565d4eee5e807f417a
Johannes Schüth <git@jotschi.de>
2026-04-13 21:28:45 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # 1. Build the project
 cd $SCRIPT_DIR
-mvn clean package -DskipTests
+time mvn -T 8 clean package -DskipTests
 
 # 2. UI
 cd $SCRIPT_DIR/loom-ui
-npm run build
+time npm run build
 
 # 3. Build the container images
-cd $SCRIPT_DIR/loom/containers && ./build-containers.sh all
+cd $SCRIPT_DIR/loom/containers && time ./build-containers.sh all
 
 # 4. Build the cortex container image
 echo "Building metaloom/cortex-server:latest ..."
-cd $SCRIPT_DIR/cortex/container && ./build-container.sh
+cd $SCRIPT_DIR/cortex/container && time ./build-container.sh
